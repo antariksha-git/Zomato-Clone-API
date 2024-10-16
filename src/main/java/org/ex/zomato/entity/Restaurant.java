d5712ef6c2b1aa3e021e5933d7a3800e004db45f
antariksha-git <heyantariksha@gmail.com>
2024-10-16 23:56:24 +0530

    Added enums for DietType and entity for Restaurant

--- src/main/java/org/ex/zomato/entity/Restaurant.java
+++ src/main/java/org/ex/zomato/entity/Restaurant.java
@@ -1,4 +1,23 @@
 package org.ex.zomato.entity;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.ex.zomato.enums.DietType;
+
+import java.util.List;
+
+@Entity
+@Table(name = "restaurants")
 public class Restaurant {
+    @Id
+    private String restaurantId;
+
+    private String name;
+    private String description;
+    private String phoneNumber;
+    private String email;
+    @ElementCollection
+    private List<DietType> dietTypes;
 }
