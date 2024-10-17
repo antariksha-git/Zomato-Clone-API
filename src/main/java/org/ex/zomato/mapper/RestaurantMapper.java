eec19dac5615916c799bb573fdc0adb7c11b7043
antariksha-git <heyantariksha@gmail.com>
2024-10-17 16:00:02 +0530

    Added mapper class for restaurant

---
+++ src/main/java/org/ex/zomato/mapper/RestaurantMapper.java
@@ -0,0 +1,27 @@
+package org.ex.zomato.mapper;
+
+import org.ex.zomato.entity.Restaurant;
+import org.ex.zomato.responseDto.RestaurantResponse;
+import org.ex.zomato.requestDto.RestaurantRequest;
+
+public class RestaurantMapper {
+
+    public static Restaurant mapToRestaurant(RestaurantRequest restaurantRequest, Restaurant restaurant) {
+        restaurant.setDescription(restaurantRequest.getDescription());
+        restaurant.setName(restaurantRequest.getName());
+        restaurant.setEmail(restaurantRequest.getEmail());
+        restaurant.setPhoneNumber(restaurant.getPhoneNumber());
+
+        return restaurant;
+    }
+
+    public static RestaurantResponse mapToRestaurantResponse(Restaurant restaurant) {
+        RestaurantResponse restaurantResponse = new RestaurantResponse();
+        restaurantResponse.setDescription(restaurant.getDescription());
+        restaurantResponse.setName(restaurant.getName());
+        restaurantResponse.setEmail(restaurant.getEmail());
+        restaurantResponse.setPhoneNumber(restaurant.getPhoneNumber());
+
+        return restaurantResponse;
+    }
+}
