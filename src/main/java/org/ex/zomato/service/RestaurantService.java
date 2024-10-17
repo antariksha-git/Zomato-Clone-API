47c1cd5ec21a6b94cddb96d6e594aaa933c0cbd0
antariksha-git <heyantariksha@gmail.com>
2024-10-17 16:02:06 +0530

    Added addRestaurant method to add the restaurant in the database

--- src/main/java/org/ex/zomato/service/RestaurantService.java
+++ src/main/java/org/ex/zomato/service/RestaurantService.java
@@ -1,4 +1,21 @@
 package org.ex.zomato.service;
 
+import org.ex.zomato.entity.Restaurant;
+import org.ex.zomato.mapper.RestaurantMapper;
+import org.ex.zomato.repository.RestaurantRepository;
+import org.ex.zomato.responseDto.RestaurantResponse;
+import org.ex.zomato.requestDto.RestaurantRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class RestaurantService {
+
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    public RestaurantResponse addRestaurant(RestaurantRequest restaurantRequest) {
+        Restaurant restaurant = restaurantRepository.save(RestaurantMapper
+                .mapToRestaurant(restaurantRequest, new Restaurant()));
+
+        return RestaurantMapper.mapToRestaurantResponse(restaurant);
+    }
 }
