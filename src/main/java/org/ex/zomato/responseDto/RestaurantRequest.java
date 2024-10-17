9655f1dd4780d7948e3de51bb9ec9b83c3c9760c
antariksha-git <heyantariksha@gmail.com>
2024-10-17 15:41:22 +0530

    Added the required fileds in responseDto

--- src/main/java/org/ex/zomato/responseDto/RestaurantRequest.java
+++ src/main/java/org/ex/zomato/responseDto/RestaurantRequest.java
@@ -1,4 +1,17 @@
 package org.ex.zomato.responseDto;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.ex.zomato.enums.DietType;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class RestaurantRequest {
+    private String name;
+    private String description;
+    private String phoneNumber;
+    private String email;
+    private List<DietType> dietTypes;
 }
