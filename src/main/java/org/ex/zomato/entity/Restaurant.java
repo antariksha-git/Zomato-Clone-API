27bcc5973be92ba06d93577426a8c5ffbc259b10
antariksha-git <heyantariksha@gmail.com>
2024-10-17 14:44:51 +0530

    Added getters and setters using lombok

--- src/main/java/org/ex/zomato/entity/Restaurant.java
+++ src/main/java/org/ex/zomato/entity/Restaurant.java
@@ -1,23 +1,36 @@
 package org.ex.zomato.entity;
 
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.ex.zomato.config.GenerateCustomId;
 import org.ex.zomato.enums.DietType;
 
 import java.util.List;
 
 @Entity
 @Table(name = "restaurants")
+@Getter
+@Setter
 public class Restaurant {
     @Id
+    @GenerateCustomId
+    @Column(name = "restaurant_id")
     private String restaurantId;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "diet_types")
     @ElementCollection
     private List<DietType> dietTypes;
 }
