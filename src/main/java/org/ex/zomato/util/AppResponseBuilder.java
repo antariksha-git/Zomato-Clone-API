86f968a2369f8b9895e40318dd26caa9fe8a05e6
antariksha-git <heyantariksha@gmail.com>
2024-10-17 16:02:52 +0530

    Added method that returns ErrorStructure

--- src/main/java/org/ex/zomato/util/AppResponseBuilder.java
+++ src/main/java/org/ex/zomato/util/AppResponseBuilder.java
@@ -4,13 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class AppResponseBuilder {
-   public <T>ResponseEntity<ResponseStructure<T>> create(HttpStatus status, String message, T data) {
+   public static <T>ResponseEntity<ResponseStructure<T>> create(HttpStatus status, String message, T data) {
        return ResponseEntity
                .status(status)
                .body(ResponseStructure.create(status, message, data));
    }
 
-    public ResponseEntity<ErrorStructure<String>> error(HttpStatus status, String message, String rootCause) {
+    public static ResponseEntity<ErrorStructure<String>> error(HttpStatus status, String message, String rootCause) {
         return ResponseEntity
                 .status(status)
                 .body(ErrorStructure.getErrorStructure(status, message, rootCause));
