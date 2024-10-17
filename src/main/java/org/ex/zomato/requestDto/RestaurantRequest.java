c61c0221e0fbd55c86c5a2153eb4a5e945c9927a
antariksha-git <heyantariksha@gmail.com>
2024-10-17 16:00:37 +0530

    Added validation for the fileds in RestaurantRequest

--- src/main/java/org/ex/zomato/requestDto/RestaurantRequest.java
+++ src/main/java/org/ex/zomato/requestDto/RestaurantRequest.java
@@ -1,5 +1,9 @@
 package org.ex.zomato.requestDto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.ex.zomato.enums.DietType;
@@ -9,9 +13,22 @@ import java.util.List;
 @Getter
 @Setter
 public class RestaurantRequest {
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Description is required")
     private String description;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\+91[0-9]{10}$", message = "Phone number must be in the format +91 followed by 10 digits")
     private String phoneNumber;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Email must be a valid format")
     private String email;
+
+    @NotEmpty(message = "At least one diet type is required")
     private List<DietType> dietTypes;
 }
